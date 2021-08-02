61ca7fdfc56c2fc5d3334648eb1bdba43909f81f
brysonS <brys1saclaus2@gmail.com>
2021-08-01 23:05:37 -0400

    reversed linked list implementation

--- ReversedLinkedList.py
+++ ReversedLinkedList.py
@@ -1,1 +1,17 @@
 print("hello")
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        while head: 
+            temp = head
+            head = head.next
+            temp.next = prev
+            prev = temp
+        return prev
